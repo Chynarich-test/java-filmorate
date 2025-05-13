cac3dcf7c052386250a403682ad3bc01c31e5425
Chynarich-test <chynarich228@gmail.com>
2025-05-13 22:15:35 +0300

    Создать апи для моделей Film и User

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/BaseController.java
@@ -0,0 +1,78 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.IIdModel;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+public abstract class BaseController<T extends IIdModel> {
+    private final Map<Long, T> elements = new HashMap<>();
+    private final Logger log;
+
+    protected BaseController(Class<T> clazz) {
+        this.log = LoggerFactory.getLogger(clazz);
+    }
+
+    @GetMapping
+    public Collection<T> findAll() {
+        return elements.values();
+    }
+
+    @PostMapping
+    public T create(@RequestBody T element) {
+
+        if (isNotValidateNewElement(element)) {
+            log.warn("Create - Ошибка валидации введенного элемента: {}", element.toString());
+            throw new ValidationException("Ошибка валидации введенного элемента");
+        }
+
+        element = modifiedBeforeAdd(element);
+
+        element.setId(getNextId());
+
+        elements.put(element.getId(), element);
+        return element;
+    }
+
+    @PutMapping
+    public T update(@RequestBody T newElement) {
+        if (isNotValidateElementValues(newElement)) {
+            log.warn("Update - Ошибка валидации введенного элемента: {}", newElement.toString());
+            throw new ValidationException("Ошибка валидации введенного элемента");
+        }
+        return Optional.ofNullable(elements.get(newElement.getId()))
+                .map(element -> {
+                    setElementValue(element, newElement);
+                    log.info("Update - успешное обновление элемента в {}", element);
+                    return element;
+                }).orElse(null);
+    }
+
+    protected abstract T modifiedBeforeAdd(T element);
+
+    //    думал сделать через рефлексию не абстрактный метод, но для меня как то тяжеловато и вызвать гетеры и сетеры
+//    через составление строки "get" + field.getName().substring(0, 1).toUpperCase() и так далее
+//    для меня выглядит как костыль какой то
+    protected abstract void setElementValue(T target, T source);
+
+    protected abstract boolean isNotValidateNewElement(@RequestBody T element);
+
+    protected abstract boolean isNotValidateElementValues(@RequestBody T element);
+
+
+    private long getNextId() {
+        long currentMaxId = elements.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
