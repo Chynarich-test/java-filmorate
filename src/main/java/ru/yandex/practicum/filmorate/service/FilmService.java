687b9bc42deb2ef6bfdbfb7f41a609e124745102
Chynarich-test <chynarich228@gmail.com>
2025-05-26 00:06:01 +0300

    Добавить проверку на наличие пользователя который лайкает

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,8 +1,12 @@
 package ru.yandex.practicum.filmorate.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
+import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 
 import java.util.Comparator;
 import java.util.List;
@@ -10,13 +14,21 @@ import java.util.List;
 @Service
 public class FilmService {
     private final InMemoryFilmStorage inMemoryFilmStorage;
+    private final InMemoryUserStorage inMemoryUserStorage;
+    private final Logger log;
 
-    public FilmService(InMemoryFilmStorage inMemoryFilmStorage) {
+    public FilmService(InMemoryFilmStorage inMemoryFilmStorage, InMemoryUserStorage inMemoryUserStorage) {
         this.inMemoryFilmStorage = inMemoryFilmStorage;
+        this.inMemoryUserStorage = inMemoryUserStorage;
+        this.log = LoggerFactory.getLogger(FilmService.class);
     }
 
     public void addLike(Long filmId, Long userId) {
         Film film = inMemoryFilmStorage.getOne(filmId);
+        if (userId == null || inMemoryFilmStorage.getOne(userId) == null) {
+            log.info("addLike - пользователь с id: {}, не найден", userId);
+            throw new NotFoundException("Добавляемый пользователь не найден");
+        }
 
         film.getLikes().add(userId);
     }
